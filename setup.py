072f46db7b07e976e1b1c41bda153a23d7c1f7dc
MajorIdiocy <nicholas.boyd@sjsu.edu>
2017-07-13 13:52:36 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ import jinja2
 def init():
     global env
     env = jinja2.Environment(loader=jinja2.FileSystemLoader(["public/html", "public/css"]))
-    global header
-    header = env.get_template("header.html")
+    global footer
+    footer = env.get_template("footer.html")
+    global head
+    head = env.get_template("head.html")
+    global home
+    home = env.get_template("home.html")
     global pkmnpage
     pkmnpage = env.get_template("pokemon.html")
     global navbar
     navbar = env.get_template("navbar.html")
-
-def renderHeader(title="Website", desc=""):
-    return header.render(title=title, desc=desc)
