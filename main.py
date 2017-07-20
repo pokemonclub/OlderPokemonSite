6f51f4feda11af65a49033da79e34ae18ec97a3f
MajorIdiocy <nicholas.boyd@sjsu.edu>
2017-07-20 14:40:34 -0700

    public

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         },
         '/static':{
             'tools.staticdir.on':True,
-            'tools.staticdir.dir': './public'
+            'tools.staticdir.dir': '/public'
         }
     }
 
