438afc9bbc27eb4496be251c9fde8fa47049fa7d
pokemonclub <pokemonpres@gmail.com>
2017-08-24 23:57:13 -0700

    big db update bug fix time

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import os, sys, cherrypy, jinja2
+import os, sys, cherrypy, jinja2, psycopg2, hashlib
 from datetime import datetime
+
+
+hostname = 'localhost'
+username = 'postgresmon'
+password = 'Pokemon Spartans San Jose'
+database = 'signups'
 
 if __name__ == "__main__":
     env = jinja2.Environment(loader=jinja2.FileSystemLoader("public/html"))
@@ -109,6 +115,53 @@ if __name__ == "__main__":
                     challonge_link = x.challonge
                     break
             return env.get_template("home.html").render(tournaments=sorted(self.tourneys, key=lambda tournament: tournament.datetime, reverse=True), regulations=sorted(self.regulations), officers=self.officers, c_link=challonge_link)
+
+        @cherrypy.expose
+        @cherrypy.tools.json_out()
+        def formValidationQuery(self):
+            conn = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
+            cur = conn.cursor()
+            cur.execute("SELECT UserName, Email, SJSU_ID FROM(
+                SELECT * FROM Accounts, Users
+                WHERE Accounts.ID = Users.ID
+            )")
+            distincts={}
+            distincts.usernames = []
+            distincts.emails = []
+            distincts.sjsuids = []
+            for triple in cur.fetchall():
+                distincts.usernames.append(triple[0])
+                distincts.emails.append(triple[1])
+                distincts.sjsuids.append(triple[2])
+            cur.close()
+            con.close()
+            return distincts
+
+        @cherrypy.expose
+        @cherrypy.tools.json_in()
+        @cherrypy.tools.json_out()
+        def dbUpdate(self):
+            json_obj = cherrypy.request.json
+            fname = json_obj['FirstName']
+            lname = json_obj['LastName']
+            email = json_obj['Email']
+            sjsuID = json_obj['SJSUID']
+            uname = json_obj['Username']
+            pword = json_obj['Password']
+            conn = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
+            cur = conn.cursor()
+
+            passwordSalt = os.urandom(24)
+            passwordHash = hashlib.sha224(passwordSalt+pword).hexdigest()
+
+            query1 = "INSERT INTO Accounts VALUES ("+uname+","+passwordSalt+","+passwordHash+")"
+            query2 = "INSERT INTO Users VALUES ("+fname+","+lname+","+email+","+sjsuID+")"
+            cur.execute(query1)
+            cur.execute(query2)
+            conn.commit()
+            cur.close()
+            con.close()
+            return true;
 
     conf = {
         '/':{
--- public/js/controller.js
+++ public/js/controller.js
@@ -1,3 +1,62 @@
+
+function pushToDB() {
+  var fname = document.getElementById('firstName');
+  var pushData = {
+    FirstName: fname,
+    LastName: document.getElementById('lastName'),
+    Email: document.getElementById('email'),
+    SJSUID: document.getElementById('sjsuid'),
+    Username: document.getElementById('user'),
+    Password: document.getElementById('pass')
+  };
+  var url = '/dbUpdate';
+  $.post({
+        contentType: 'application/json',
+        dataType: 'json',
+        data: JSON.stringify(pushData),
+        url: url,
+        error:function() {
+            alert('Sorry, an error occurred. Please contact Nick, and try again later.');
+        },
+        success:function(response) {
+            alert("Thanks "+ fname +"! You've been added to our user database! If you have any questions, please ask Nick.");
+        }
+    });
+}
+
+function submitSignup() {
+  if(document.getElementById('sjsuid').value.length!=9){
+    alert("Please enter your nine digit ID, or nine zeroes for ID if you're not a student.");
+    return false;
+  } else if (document.getElementById('pass') != document.getElementById('passconfirm')) {
+    alert("Passwords do not match.");
+    return false;
+  }
+  var request = $.ajax({'url': '/formValidationQuery'});
+  request.done(function(response){
+    var data = JSON.parse(response);
+    var uname = document.getElementById('user');
+    var email = document.getElementById('email');
+    var id = document.getElementById('sjsuid');
+    if(data.usernames.indexOf(uname)>=0){
+      alert("Username already taken. Please try another!");
+      return false;
+    } else if(data.emails.indexOf(email)>=0) {
+      alert("Email already registered. Please contact Nick for assistance with account recovery!");
+      return false;
+    } else if(data.sjsuids.indexOf(id)>=0) {
+      alert("SJSU ID already registered. Please contact Nick for assistance with account recovery!");
+      return false;
+    }
+    pushToDB();
+    return true;
+  });
+  request.fail(function(jqXHR, textStatus){
+    alert("Request failed, contact Nick with the following: " + textStatus);
+    return false;
+  });
+}
+
 $(document).ready(function() {
   $(".navbar-nav li").on("click", function(){
     $(".nav").find(".active").removeClass("active");
